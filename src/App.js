44c8691a4e62444b3b94fb8d955feadbaaf62bd3
Jagrut Sharma <101259156+jagrut-sharma@users.noreply.github.com>
2022-08-13 20:27:23 +0530

    added elements

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1 className="title">
+        <span>🎧</span> Sweet Music <span>🎼</span>
+      </h1>
+      <p className="para">Here is some of the great music to listen to:</p>
+      <button className="btn">Chill</button>
+      <button className="btn">Romantic</button>
+      <button className="btn">Travel</button>
+      <button className="btn">Energetic</button>
+      <button className="btn">Fun</button>
+
+      <div className="container"></div>
     </div>
   );
 }
