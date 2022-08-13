ae7d95050f3c1d0c59aadc2e68d8d75c15cf39ea
Jagrut Sharma <101259156+jagrut-sharma@users.noreply.github.com>
2022-08-13 20:37:16 +0530

    style: added pointers and functions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import "./styles.css";
 
 export default function App() {
+  function clickHandler(button) {
+    console.log(button);
+  }
+
   return (
     <div className="App">
       <h1 className="title">
         <span>🎧</span> Sweet Music <span>🎼</span>
       </h1>
       <p className="para">Here is some of the great music to listen to:</p>
-      <button className="btn">Chill</button>
-      <button className="btn">Romantic</button>
-      <button className="btn">Travel</button>
-      <button className="btn">Energetic</button>
-      <button className="btn">Fun</button>
+      <button
+        style={{ cursor: "pointer" }}
+        className="btn"
+        onClick={() => clickHandler("chill")}
+      >
+        Chill
+      </button>
+      <button
+        style={{ cursor: "pointer" }}
+        className="btn"
+        onClick={() => clickHandler("romantic")}
+      >
+        Romantic
+      </button>
+      <button
+        style={{ cursor: "pointer" }}
+        className="btn"
+        onClick={() => clickHandler("travel")}
+      >
+        Travel
+      </button>
+      <button
+        style={{ cursor: "pointer" }}
+        className="btn"
+        onClick={() => clickHandler("sad")}
+      >
+        Sad
+      </button>
+      <button
+        style={{ cursor: "pointer" }}
+        className="btn"
+        onClick={() => clickHandler("fun")}
+      >
+        Fun
+      </button>
 
-      <div className="container"></div>
+      <div className="container">
+        <p></p>
+      </div>
     </div>
   );
 }
